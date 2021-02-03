5c8bcb4e56b3788490c8d12919655843e19c3ee1
Hasan Gharaibeh <swehu1988@gmail.com>
2021-02-03 14:52:32 +1100

    Triangle base class added

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Triangle triangle = new Triangle();
+                printResult(triangle.getArea(3,4,5));
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.ReadLine();
+        }
+
+        static void printResult(float output)
+        {
+            Console.WriteLine("The area for this triangle is:\n" + output);
         }
     }
 }
---
+++ ConsoleApp1/ConsoleApp1/Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Triangle
+    {
+
+        public float getArea(float a, float b, float c)
+        {
+            float area = 0.0f;
+
+
+            return area;
+        }
+    }
+}
